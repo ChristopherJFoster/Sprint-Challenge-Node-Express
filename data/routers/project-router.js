129172054b2705300b957ed8d7689b86d2629548
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-03-22 12:02:12 -0400

    GET a specific project now works.

--- data/routers/project-router.js
+++ data/routers/project-router.js
@@ -2,19 +2,6 @@ const router = require('express').Router();
 
 const projectModel = require('../helpers/projectModel');
 
-// router.get('/', (req, res) => {
-//   projectModel
-//     .get()
-//     .then(users => {
-//       res.status(200).json(users);
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: 'The users information could not be retrieved.' });
-//     });
-// });
-
 router.get('/', async (req, res) => {
   try {
     let projects = await projectModel.get();
@@ -26,24 +13,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-// router.get('/:id', (req, res) => {
-//   projectModel
-//     .getById(req.params.id)
-//     .then(user => {
-//       if (user) {
-//         res.status(200).json(user);
-//       } else {
-//         res
-//           .status(404)
-//           .json({ error: 'The user with the specified ID does not exist.' });
-//       }
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: 'The user information could not be retrieved.' });
-//     });
-// });
+router.get('/:id', async (req, res) => {
+  try {
+    let project = await projectModel.get(req.params.id);
+    res.status(200).json(project);
+  } catch (err) {
+    res.status(404).json({
+      error:
+        'There is no project with that ID, or the project information could not be retrieved.'
+    });
+  }
+});
 
 // router.post('/', (req, res) => {
 //   if (req.body.name) {
